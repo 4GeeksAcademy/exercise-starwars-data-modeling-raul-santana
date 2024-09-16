a69aa3233c16f63f61af62db5197e5fd3c647013
Raúl Santana Castilla <168531613+RaulSantanaCastilla@users.noreply.github.com>
2024-09-16 11:50:13 +0000

    Trabajo terminado

--- src/models.py
+++ src/models.py
@@ -16,16 +16,40 @@ class User(Base):
     lastName = Column(String(30), nullable=False)
     email = Column(String(40), unique=True, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    charactersId = Column(Integer, ForeignKey ('characters.id'))
+    planetsId = Column(Integer, ForeignKey ('planets.id'))
+    starshipsId = Column(Integer, ForeignKey ('starships.id'))
+    vehiclesId = Column(Integer, ForeignKey ('vehicles.id'))
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25))
+    birthYear = Column(String(25))
+    species = Column(String(25))
+    height = Column(String(25))
+    mass = Column(String(25))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25))
+    population = Column(String(25))
+    orbitalPeriod = Column(String(25))
+    diameter = Column(String(25))
+    gravity = Column(String(25))
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25))
+    model = Column(String(25))
+    manufacturer = Column(String(25))
+    speed = Column(String(25))
+
 
     def to_dict(self):
         return {}
